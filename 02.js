eba3aded64500b5486ffaef63f03c210b9286b1f
danapipi <danapipi18@gmail.com>
2018-06-10 15:32:59 +0700

    first commit

---
+++ 02.js
@@ -0,0 +1,7 @@
+const first = ['Behind', 'every', 'great', 'man']
+const second = ['is', 'a', 'woman']
+const third = ['rolling', 'her', 'eyes']
+const all = first.concat(second).concat(third);
+
+
+console.log(all.join(" "));
